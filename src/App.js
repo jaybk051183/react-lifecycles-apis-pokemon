c535317543887897e430ad9d8e106f71ff3f1b54
jaybk051183 <kailashbhaggoe@outlook.com>
2022-09-27 20:43:38 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import axios from "axios";
+
 
 function App() {
+
   return (
     <div>
-      Begin hier met de opdracht!
+      Hello
     </div>
   );
 }
